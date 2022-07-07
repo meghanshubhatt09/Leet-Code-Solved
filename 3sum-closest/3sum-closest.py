468593ff4a02b7364dc587e57b3332a4d29d9b73
meghanshubhatt09 <98858327+meghanshubhatt09@users.noreply.github.com>
2022-07-07 13:55:39 -0400

    Time: 7452 ms, Memory: 14.1 MB - LeetHub

---
+++ 3sum-closest/3sum-closest.py
@@ -0,0 +1,43 @@
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        
+        nums.sort()
+        result = prev_difference = 10000
+        
+        
+        
+        for i in range(len(nums)):
+            
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            start = i+1
+            end = len(nums) - 1
+            
+            while(start < end):
+                sumx = nums[i] + nums[start] + nums[end]
+                
+                if sumx == target:
+                    return sumx
+                
+                
+                difference = abs(target - sumx)
+                if difference < prev_difference:
+                    result = sumx  
+                    prev_difference = difference
+                    
+                if sumx > target:
+                    end -= 1
+                elif sumx < target:
+                    start += 1
+                    
+        return result
+                
+
+                
+                
+                
+                
+                
+                
+                
+        
