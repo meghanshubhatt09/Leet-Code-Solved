b6130f67273298083be3b7689a06f3d532fb0c1d
meghanshubhatt09 <98858327+meghanshubhatt09@users.noreply.github.com>
2022-07-20 13:40:12 -0400

    Time: 45 ms (76.36%), Space: 13.9 MB (83.08%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.py
@@ -0,0 +1,34 @@
+import re
+
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        
+        i = 0
+        new_string = re.sub(r'[^\w\s]', ' ', paragraph)
+        mylst = new_string.lower().split(' ')
+        j = 0
+        banned_new = [i.lower() for i in banned]
+        word_Set = dict()
+        print(mylst)
+        for i in mylst:
+            key = i
+            if key not in banned_new and len(key)>0:
+                if key not in word_Set:
+                    word_Set[key] = 1
+                else:
+                    word_Set[key] += 1
+        max_value = 0      
+        word = ''
+        for key,value in word_Set.items():
+            if value > max_value:
+                max_value = value
+                word = key
+        return word
+                
+                
+            
+
+            
+        
+        
